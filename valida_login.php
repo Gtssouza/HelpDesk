88a99f9bc3fe31be8c6f0ba32ec24484f6908e17
Gustavo Souza <gtssouza94@gmail.com>
2020-09-30 13:08:13 -0300

    Validando login e senha

--- valida_login.php
+++ valida_login.php
@@ -10,12 +10,32 @@
 
 */
 
-print_r($_POST);
+//relação de usuarios estatica
 
-echo '<br />';
+//variavel que verifica user autenticado
+$usuario_autenticado = false;
 
-    echo $_POST['email'];
-    echo '<br />';
-    echo $_POST['senha'];
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcde')
+);
+
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+
+foreach($usuarios_app as $user){
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+    
+}
+
+if($usuario_autenticado){
+    echo 'User autenticado';
+}else{
+    header('Location: index.php?login=erro');
+}
+
 
 ?>
